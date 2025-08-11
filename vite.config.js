462fb19fe1db114aed65c7a9627b7c8904ab5a02
Mylène Benier-Rollet <mylenebenierrollet@MacBook-Pro-de-Mylene.local>
2025-08-11 09:34:15 +0200

    CV3D : add vite.config for star1.html

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
+import { resolve } from 'path';
+
 /**
  * @type {import('vite').UserConfig}
  */
 export default {
-    base: process.env.NODE_ENV === 'production' ? '/cv_3d/' : ''
+    base: process.env.NODE_ENV === 'production' ? '/cv_3d/' : '',
+    build: {
+        rollupOptions: {
+        input: {
+            main: resolve(__dirname, 'index.html'),
+            s1: resolve(__dirname, 'star1.html'),
+        }
+        }
+    }
 }
