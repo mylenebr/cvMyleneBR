519cf44039ac27bb8c4a5e1f4c021ae0e16961d4
Mylène Benier-Rollet <mylenebenierrollet@Mac.lan>
2025-08-21 16:31:05 +0200

    CV3D: Interet -> Add basic stuff

--- interets.js
+++ interets.js
@@ -16,6 +16,60 @@ let textMesh, buttonMesh, frameMesh;
 
 init();
 
+// Util
+function createRoundedRectShape(width, height, radius) {
+	const x = -width / 2;
+	const y = -height / 2;
+
+	const shape = new THREE.Shape();
+	shape.moveTo(x + radius, y);
+	shape.lineTo(x + width - radius, y);
+	shape.quadraticCurveTo(x + width, y, x + width, y + radius);
+	shape.lineTo(x + width, y + height - radius);
+	shape.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+	shape.lineTo(x + radius, y + height);
+	shape.quadraticCurveTo(x, y + height, x, y + height - radius);
+	shape.lineTo(x, y + radius);
+	shape.quadraticCurveTo(x, y, x + radius, y);
+
+	return shape;
+}
+
+function addText(text, size, depth, height, curveSegments,
+	color, pos)
+{
+	const loaderHeader = new FontLoader();
+	loaderHeader.load(
+		'fonts/optimer_bold.typeface.json',
+		function (font) {
+			const textGeo = new TextGeometry(text, {
+				font: font,
+				size: size,
+				depth: depth,
+				height: height,
+				curveSegments: curveSegments
+			});
+			textGeo.center();
+			
+			//const textMat = new THREE.MeshBasicMaterial({ color: 0xffffff });
+			const textMat = new THREE.MeshPhongMaterial({color: color});
+			textMesh = new THREE.Mesh(textGeo, textMat);
+			textMesh.position.set(pos.x, pos.y, pos.z);
+
+			// Attach text to star so it moves/rotates with it
+			scene.add(textMesh);
+		},
+		// onProgress callback
+		function ( xhr ) {
+			console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+		},
+		// onError callback
+		function ( err ) {
+			console.log( 'An error happened' );
+		}
+	);
+}
+
 function title()
 {
 
@@ -70,6 +124,98 @@ function homePageButton()
 	scene.add(frameMesh);
 }
 
+function addMusic()
+{
+	// Frame
+	const shape = createRoundedRectShape(200, 180, 10);
+	const geometry = new THREE.ShapeGeometry(shape);
+	const material = new THREE.MeshStandardMaterial({ color: darkPink, side: THREE.DoubleSide });
+	const roundedPlaneMesh = new THREE.Mesh(geometry, material);
+	roundedPlaneMesh.position.set(-250, -60, 9.9);
+	scene.add(roundedPlaneMesh);
+
+	// Picture
+	const texture = new THREE.TextureLoader().load(`textures/pictures/hobbies/music.JPG`);
+	const logoGeometry = new THREE.CircleGeometry(30, 32);
+	const buttonMaterial = new THREE.MeshBasicMaterial({map: texture, transparent: false });
+	const logoMesh = new THREE.Mesh(logoGeometry, buttonMaterial);
+	logoMesh.position.set(-300, 40, 10); // put it in front of camera
+	scene.add(logoMesh); 
+
+	// Text
+	const musicText = "La majorité de mon temps libre \nest consacrée à la musique : \nchant, guitare, piano, \ncomposition, production, etc. \nJouer avec des amis ou seule, \nsur scène ou dans ma chambre, \ntout ce qui y est attrait \nme fais vibrer.";
+	addText(musicText, 8, 1, 0, 8, lightPink, new THREE.Vector3(-250, -60, 10));
+}
+
+function addBooks()
+{
+	// Frame
+	const shape = createRoundedRectShape(200, 80, 10);
+	const geometry = new THREE.ShapeGeometry(shape);
+	const material = new THREE.MeshStandardMaterial({ color: darkPink, side: THREE.DoubleSide });
+	const roundedPlaneMesh = new THREE.Mesh(geometry, material);
+	roundedPlaneMesh.position.set(0, -12, 9.9);
+	scene.add(roundedPlaneMesh);
+
+	// Picture
+	const texture = new THREE.TextureLoader().load(`textures/pictures/hobbies/books.jpg`);
+	const logoGeometry = new THREE.CircleGeometry(15, 32);
+	const buttonMaterial = new THREE.MeshBasicMaterial({map: texture, transparent: false });
+	const logoMesh = new THREE.Mesh(logoGeometry, buttonMaterial);
+	logoMesh.position.set(70, -30, 10); // put it in front of camera
+	scene.add(logoMesh); 
+
+	// Text
+	const musicText = "Un autres de mes passe temps \nfavoris est la lecture, un temps \nd'attente ? J'ai toujours un \nlivre dans mon sac a sortir \npour le remplir !";
+	addText(musicText, 7, 1, 0, 8, lightPink, new THREE.Vector3(-10, -12, 10));
+}
+
+function addAsso()
+{
+	// Frame
+	const shape = createRoundedRectShape(200, 85, 10);
+	const geometry = new THREE.ShapeGeometry(shape);
+	const material = new THREE.MeshStandardMaterial({ color: darkPink, side: THREE.DoubleSide });
+	const roundedPlaneMesh = new THREE.Mesh(geometry, material);
+	roundedPlaneMesh.position.set(0, -112, 9.9);
+	scene.add(roundedPlaneMesh);
+
+	// Picture
+	const texture = new THREE.TextureLoader().load(`textures/pictures/hobbies/asso.jpg`);
+	const logoGeometry = new THREE.CircleGeometry(15, 32);
+	const buttonMaterial = new THREE.MeshBasicMaterial({map: texture, transparent: false });
+	const logoMesh = new THREE.Mesh(logoGeometry, buttonMaterial);
+	logoMesh.position.set(70, -130, 10); // put it in front of camera
+	scene.add(logoMesh); 
+
+	// Text
+	const musicText = "J'aime egalement m'impliquer dans la vie \nassociative. En ecole d'ingenieur j'etais \npresidente de plusieurs assos/club. \nEt tout au long de mes etudes j'ai \nparticipe dans plusieurs clubs \nsportifs et de musique.";
+	addText(musicText, 6, 1, 0, 8, lightPink, new THREE.Vector3(-10, -112, 10));
+}
+
+function addSpanish()
+{
+	// Frame
+	const shape = createRoundedRectShape(200, 180, 10);
+	const geometry = new THREE.ShapeGeometry(shape);
+	const material = new THREE.MeshStandardMaterial({ color: darkPink, side: THREE.DoubleSide });
+	const roundedPlaneMesh = new THREE.Mesh(geometry, material);
+	roundedPlaneMesh.position.set(250, -60, 9.9);
+	scene.add(roundedPlaneMesh);
+
+	// Picture
+	const texture = new THREE.TextureLoader().load(`textures/pictures/hobbies/spain.png`);
+	const logoGeometry = new THREE.CircleGeometry(30, 32);
+	const buttonMaterial = new THREE.MeshBasicMaterial({map: texture, transparent: false });
+	const logoMesh = new THREE.Mesh(logoGeometry, buttonMaterial);
+	logoMesh.position.set(300, -140, 10); // put it in front of camera
+	scene.add(logoMesh); 
+
+	// Text
+	const spainText = "Puisque que j'aime toujours \napprendre, j'ai récement décidé \nd'étudier l'espagnol. J'ai choisis \ncette langue pour deux raisons \nmajeurs: c'est une langue parlée \ndans beaucoup de pays (que j'aimerais \nvisiter un jour), et la culture\nhispanique m'intéresse de plus en plus, \nen particulier (vous l'aurez peut-etre \ndevine) la musique \nhispanophone !";
+	addText(spainText, 7, 1, 0, 8, lightPink, new THREE.Vector3(250, -60, 10));
+}
+
 function init() {
 
 	// Renderer
@@ -101,6 +247,18 @@ function init() {
 	title();
 	homePageButton();
 
+	// Music
+	addMusic();
+
+	// Books
+	addBooks();
+
+	// Asso
+	addAsso();
+
+	// Spanish
+	addSpanish();
+
 	// Raycaster setup
 	raycaster = new THREE.Raycaster();
 	mouse = new THREE.Vector2();
@@ -123,6 +281,8 @@ function onClick(event) {
 
 function animate() {
     requestAnimationFrame(animate);
+	controls.update();
+
   	renderer.render(scene, camera);
 }
 renderer.setAnimationLoop( animate );
